7b379ec983b1273ae4eaa2a7b337ee6d6559922e
yeesuho <suho080214@gmail.com>
2025-07-30 23:22:03 +0900

    Javascript Array의 문제점

---
+++ 6_array/1_problems_with_array_in_js.ts
@@ -0,0 +1,39 @@
+/**
+ * Problems with Array in JS
+ */
+const number = [1, '2', 3, '4', 5];
+
+let strings: string[] = ['1', '2', '3'];
+
+// strings.push(1);
+
+let stringsOrNumbersArray: (string | number)[] = [1, '2', 3, '4'];
+
+let stringArrNumberArr: string[] | number[] = [1, 2, 3]
+
+let stringOrNumberArr: string | number[] = [
+    1,2,3
+]
+
+stringOrNumberArr = '3';
+
+let boolsArr = [true, false, true];
+
+boolsArr.push(false);
+
+// boolsArr.push(1);
+
+const onlyString = ['1', '2', '3'];
+const onlyNumbers = [1, 2, 3];
+
+for(let i = 0; i < onlyString.length; i++) {
+    let item = onlyString[i];
+}
+
+for(let item of onlyNumbers) {
+
+}
+
+let number3 = onlyNumbers[0];
+
+let number4 = onlyNumbers[9999]; // 타입스크립트는 튜플이 아닌 이상 배열의 길이를 신경쓰지 않음
